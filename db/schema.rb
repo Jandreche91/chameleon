2ae7bd89717f9266632fc72e05aa410cfe9637b2
javier echeverri <jecheverrid@gmail.com>
2020-06-25 20:36:56 +0200

    assignment model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_180834) do
+ActiveRecord::Schema.define(version: 2020_06_25_182544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_assignments_on_project_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.text "description"
