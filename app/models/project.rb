e9a3400065687b283c1088fa61d4cd9072d95a4a
javier echeverri <jecheverrid@gmail.com>
2020-06-25 20:49:28 +0200

    alert model added

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   validates :name, :description, :estimated_start_date, :estimated_end_date, :estimated_cost, presence: true
   belongs_to :user
   has_many :assignments
+  has_many :alerts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_182544) do
+ActiveRecord::Schema.define(version: 2020_06_25_184429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alerts", force: :cascade do |t|
+    t.bigint "project_id"
+    t.string "description"
+    t.boolean "done", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_alerts_on_project_id"
+  end
 
   create_table "assignments", force: :cascade do |t|
     t.bigint "user_id"
