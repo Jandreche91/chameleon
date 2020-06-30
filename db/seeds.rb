46643f4e3664d488aa6cdb4fd9c2d22ea7036425
javier echeverri <jecheverrid@gmail.com>
2020-06-30 13:59:55 +0200

    added a manager and a team to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,52 @@ puts "All tasks destroyed ❗"
 User.destroy_all
 puts "All users destroyed ❗"
 
-# Creation of manager
+############## Creation of manager ########################################################
+###########################################################################################
 
 puts "🌱 Creation of manager starts 🌱"
 
-manager = User.new( unsername: "john_smith",
+manager = User.new( username: "john_smith",
                     first_name: "John",
                     last_name: "Smith",
                     hourly_rate: 750,
                     email: "john.smith@our_team.org",
                     password:"123456")
 puts "🌱 Manager has User id #{manager.id} and was successfully saved! 🌱" if manager.save
+
+###########################################################################################
+####################### CREATION OF THE TEAM ##############################################
+###########################################################################################
+
+puts "🌱 Creation of the team starts 🌱"
+
+team_hash = { "Nancy"=>["Yost", 650],
+ "Jarrod"=>["Mante", 650],
+ "Joan"=>["Mraz", 558],
+ "Stepanie"=>["Murphy", 450],
+ "Theodore"=>["Fisher", 440],
+ "Pedro"=>["Ortiz", 330],
+ "Chad"=>["Pfeffer", 330],
+ "Grace"=>["Olson", 300],
+ "Vincenzo"=>["Williams", 450],
+ "Christina"=>["Tillman", 250],
+ "Andreas"=>["Schumm", 200],
+ "Antoine" => ["Konopelski", 150],
+ "Abdul"=>["Saheem", 100],
+ "Levi"=>["Schmeler", 100],
+  "Rodney"=>["Weber", 280],
+  "Frances"=>["Predovic", 350],
+  "Julian"=>["McNelly", 120]
+}
+
+team_hash.each do |name, values|
+  last_name = values[0]
+  hourly_rate = values[1]
+  associate = User.new( username: "#{name.downcase}_#{last_name.downcase}",
+                    first_name: name,
+                    last_name: last_name,
+                    hourly_rate: hourly_rate,
+                    email: "#{name.downcase}.#{last_name.downcase}@our_team.org",
+                    password:"123456")
+  puts "🌱 Profile for #{associate.first_name + " " + associate.last_name} created successfully 🌱" if associate.save
+end
