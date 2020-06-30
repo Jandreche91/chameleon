e2de12f3ca91880b97c3642a9693d2cce8c3aed3
Jey Val <jeremv@gmail.com>
2020-06-30 21:32:59 +0200

    routes controllers views of projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,45 @@
+class ProjectsController < ApplicationController
+  # correspond a tous les projets
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    @project.save
+    redirect_to project_path(@project)
+  end
+
+  def show
+    # projects/3
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :estimated_start_date, :estimated_end_date, :estimated_cost)
+  end
+
+end
