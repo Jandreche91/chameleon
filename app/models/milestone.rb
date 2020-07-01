5dab23e0e4e192a2a0921883d656e93f1d9779d7
javier echeverri <jecheverrid@gmail.com>
2020-07-01 10:05:50 +0200

    add methods in models to fetch data more easily in front-end

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -2,4 +2,16 @@ class Milestone < ApplicationRecord
   belongs_to :project
   has_many :tasks, dependent: :destroy
   validates :description, :end_date, :progress_rate, presence: true
+
+  def executed_value
+    sum = 0
+    tasks.each do |task|
+      sum += task.value
+    end
+    sum
+  end
+
+  def hours_spent
+    tasks.sum(:hours_spent)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :milestone
   validates :description, :date, :hours_spent, presence: true
+
+  def value
+    User.find(user_id).hourly_rate * hours_spent
+  end
 end
