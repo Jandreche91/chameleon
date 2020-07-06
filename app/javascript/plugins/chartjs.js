504d8dfa4b0666fb7b1a7b6fe203e3d289251ee3
javier echeverri <jecheverrid@gmail.com>
2020-07-06 11:51:36 +0200

    changes in the X axis so it whows the month names

--- app/javascript/plugins/chartjs.js
+++ app/javascript/plugins/chartjs.js
@@ -1,27 +1,49 @@
 
-require("chart.js")
+require("chart.js");
+require("datejs");
 
-console.log("Chartjs page is loading!");
+// Function that constructs an array with the past X months to be shown on the X Axis
+
+const constructXAxisMonths = (months) => {
+  results = [];
+  date = Date.parse(Date.today());
+  for (let i =1; i <= months; i++) {
+    month = date.toString('MMM yy');
+    results.push(month);
+    date = Date.parse(date).add(-1).month();
+  }
+  return results.reverse()
+};
+
 
 let overallPerformanceChart = document.getElementById('overallPerformance');
-console.log(overallPerformanceChart.dataset)
+// console.log(overallPerformanceChart.dataset)
 const overallPerformance = new Chart(overallPerformanceChart, {
   type: 'line',
   data: {
-    labels: [1,2,3,4,5,6,7,8,9,10,11,12],
+    labels: constructXAxisMonths(12),
     datasets: [{
       label: 'Billed',
       data: JSON.parse(overallPerformanceChart.dataset.value),
       borderColor: ['rgba(170, 239, 104, 0.9)'],
       backgroundColor: ['rgba(170, 239, 104, 0.0)'],
-      // backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-      // borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
       borderWidth: 4,
       pointRadius: 0.1,
-      pointHitRadius: 2
+      pointHitRadius: 5
     }]
   },
   options: {
+    legend: {
+      position: "bottom"
+    },
+    layout: {
+            padding: {
+                left: 25,
+                right: 25,
+                top: 25,
+                bottom: 25
+            }
+        },
     scales: {
       yAxes: [{
         ticks: {
