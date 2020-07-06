c5b07bddb45dfb6335787c25121be535e72f76b3
javier echeverri <jecheverrid@gmail.com>
2020-07-06 11:13:47 +0200

    some changes to the aesthetics of the overall performance table

--- app/javascript/plugins/chartjs.js
+++ app/javascript/plugins/chartjs.js
@@ -5,16 +5,20 @@ console.log("Chartjs page is loading!");
 
 let overallPerformanceChart = document.getElementById('overallPerformance');
 console.log(overallPerformanceChart.dataset)
-let myChart = new Chart(overallPerformanceChart, {
+const overallPerformance = new Chart(overallPerformanceChart, {
   type: 'line',
   data: {
     labels: [1,2,3,4,5,6,7,8,9,10,11,12],
     datasets: [{
       label: 'Billed',
       data: JSON.parse(overallPerformanceChart.dataset.value),
+      borderColor: ['rgba(170, 239, 104, 0.9)'],
+      backgroundColor: ['rgba(170, 239, 104, 0.0)'],
       // backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
       // borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-      borderWidth: 1
+      borderWidth: 4,
+      pointRadius: 0.1,
+      pointHitRadius: 2
     }]
   },
   options: {
