c24dbfb298ecc3c2d1518e3d0b5f80f5e7a3fc1e
javier echeverri <jecheverrid@gmail.com>
2020-07-13 10:17:37 +0200

    assignments for new project debugged

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,13 +14,13 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-
     @project.user = current_user
     if @project.save
-      p "test if"
+      users_assigned
+      @users_assigned = params[:project][:user_id]
+      create_assignments(@users_assigned, @project)
       redirect_to project_path(@project)
     else
-      p "test else"
       render :new
     end
   end
@@ -53,11 +53,27 @@ class ProjectsController < ApplicationController
 
   private
 
+  # users_assigned is an array of ids as strings
+  def create_assignments(users_assigned, project)
+    users_assigned.each do |user_id|
+      next if user_id == ""
+
+      assignment = Assignment.new
+      assignment.user = User.find(user_id.to_i)
+      assignment.project = project
+      assignment.save
+    end
+  end
+
   def find_a_project
     @project = Project.find(params[:id])
   end
 
   def project_params
-    params.require(:project).permit(:name, :description, :estimated_start_date, :estimated_end_date, :estimated_cost)
+    params.require(:project).permit(:name, :description, :estimated_start_date, :estimated_end_date, :estimated_cost, :user_id)
+  end
+
+  def users_assigned
+    params.require(:project).permit(:user_id)
   end
 end
