36c03bc078a51c839e29dec3874ffde19f44c18e
javier echeverri <jecheverrid@gmail.com>
2020-07-14 09:47:52 +0200

    revamp of tabs in project#index and new variable with descriptions as class variable

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,6 +1,18 @@
 class Alert < ApplicationRecord
   belongs_to :project
   validates :description, presence: true
+  attr_reader :descriptions
+
+  def initialize
+    @descriptions = {
+      budget_surpassed_85: "has surpassed 85 percent of the budget",
+      budget_surpassed_50: "has surpassed 50 percent of the budget",
+      progress_excess_time: "progress until end date is significantly higher than budget",
+      progress_excess_budget: "budget spent is significantly higher than progress until end date",
+      user_little_work: "is below the average billing time of your team",
+      user_lots_works: "is above the average billing time of your team"
+    }
+  end
 
   def self.outstanding
     Alert.where(done: false)
