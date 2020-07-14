a18e6f7419b626354b055c55f79124121748bc9a
javier echeverri <jecheverrid@gmail.com>
2020-07-14 10:50:38 +0200

    basic logic of logic for alerts has been implemented

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,22 +2,39 @@ class Alert < ApplicationRecord
   belongs_to :project
   validates :description, presence: true
 
-  def descriptions
-    {
-      budget_surpassed_85: "has surpassed 85 percent of the budget",
-      budget_surpassed_50: "has surpassed 50 percent of the budget",
-      progress_excess_time: "progress until end date is significantly higher than budget",
-      progress_excess_budget: "budget spent is significantly higher than progress until end date",
-      user_little_work: "is below the average billing time of your team",
-      user_lots_works: "is above the average billing time of your team"
-    }
-  end
-
   def self.outstanding
     Alert.where(done: false)
   end
 
-  def self.generate
+  def self.descriptions
+    {
+      budget_surpassed_85: "has surpassed 85 percent of the budget",
+      budget_surpassed_50: "has surpassed 50 percent of the budget",
+      progress_excess_time: "progress until end date is significantly higher than budget",
+      progress_excess_budget: "budget spent is significantly higher than progress until end date"
+    }
+  end
 
+  def self.generate
+    Project.all.each do |project|
+      generate_alerts_budget(project)
+    end
+  end
+
+  def self.generate_alerts_budget(project)
+    alerts = Alert.descriptions
+    excess_budget = project.executed_budget_percentage - project.progress_date_percentage > 0.1
+    excess_time = project.progress_date_percentage - project.executed_budget_percentage > 0.1
+    surpass50 = project.executed_budget_percentage > 0.5
+    surpass85 = project.executed_budget_percentage > 0.85
+    if excess_budget && project.alerts.where(description: alerts[:progress_excess_budget]).empty?
+      Alert.create(description: alerts[:progress_excess_budget], project: project)
+    elsif excess_time && project.alerts.where(description: alerts[:progress_excess_time]).empty?
+      Alert.create(description: alerts[:progress_excess_time], project: project)
+    elsif surpass50 && project.alerts.where(description: alerts[:budget_surpassed_50]).empty?
+      Alert.create(description: alerts[:budget_surpassed_50], project: project)
+    elsif surpass85 && project.alerts.where(description: alerts[:budget_surpassed_85]).empty?
+      Alert.create(description: alerts[:budget_surpassed_85], project: project)
+    end
   end
 end
