c868022bb60f80969192a630dd99f53d65072482
javier echeverri <jecheverrid@gmail.com>
2020-07-18 16:12:17 +0200

    new milestone form formatted

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -12,6 +12,6 @@ class Milestone < ApplicationRecord
   end
 
   def self.generate_colors
-    ['#7900B3', '#006BF0', '#338508', '#B37600', '#FFC65A', '#1E334D', '#B30000', '#00B392', '#AAEF68']
+    ['#7900B3', '#006BF0', '#338508', '#B30000', '#B37600', '#1E334D', '#FFC65A', '#00B392', '#AAEF68']
   end
 end
