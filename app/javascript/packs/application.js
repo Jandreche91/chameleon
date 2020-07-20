55aa9a5dcf8f2c62a9e999dc3b81926d420ed282
ellimca <mister_lemon@hotmail.com>
2020-07-20 22:36:12 +0200

    sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("datejs");
 
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -90,3 +91,13 @@ document.addEventListener('turbolinks:load', () => {
 
   initProgressBar();
 });
+
+  // sidebar
+
+  $(document).ready(function(){
+    $("#menu-toggle").click(function(e){
+      e.preventDefault();
+      $("#wrapper").toggleClass("menuDisplayed");
+    });
+  });
+
